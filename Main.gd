ecfdbc0812729f154ef963c8a82fe4ddcbc0bc97
aleksandr-s-g <a.s.gavrilov94@gmail.com>
2023-04-29 02:16:02 +0400

    1

--- Main.gd
+++ Main.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	get_tree().get_root().connect("size_changed", resized)
 	pass # Replace with function body.
 
 
@@ -14,3 +15,6 @@ func _process(delta):
 func _on_item_rect_changed():
 	print("size changed")
 	pass # Replace with function body.
+	
+func resized():
+	print("size changed to ", get_viewport().get_visible_rect().size)
