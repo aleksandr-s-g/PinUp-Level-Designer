bae08cab2a944e674efc62ea15ea0fd93d75fd8f
aleksandr-s-g <a.s.gavrilov94@gmail.com>
2023-06-03 16:16:55 +0400

    add coins managering

--- WorkField.gd
+++ WorkField.gd
@@ -2,11 +2,12 @@ extends Node2D
 var work_height = 0
 var work_width = 0
 var cell_size = 0
-var cur_lab_part = {'in':0,'out':0, 'blocks':[]}
-var full_lab_part = {"blocks":[{"colour":"blue","x":9,"y":19},{"colour":"blue","x":8,"y":19},{"colour":"blue","x":7,"y":19},{"colour":"blue","x":6,"y":19},{"colour":"blue","x":5,"y":19},{"colour":"red","x":4,"y":19},{"colour":"red","x":3,"y":19},{"colour":"red","x":2,"y":19},{"colour":"red","x":1,"y":19},{"colour":"red","x":0,"y":19},{"colour":"red","x":0,"y":18},{"colour":"red","x":1,"y":18},{"colour":"red","x":2,"y":18},{"colour":"red","x":3,"y":18},{"colour":"red","x":4,"y":18},{"colour":"blue","x":5,"y":18},{"colour":"blue","x":6,"y":18},{"colour":"blue","x":7,"y":18},{"colour":"blue","x":8,"y":18},{"colour":"blue","x":9,"y":18},{"colour":"blue","x":9,"y":17},{"colour":"blue","x":8,"y":17},{"colour":"blue","x":7,"y":17},{"colour":"blue","x":6,"y":17},{"colour":"blue","x":5,"y":17},{"colour":"red","x":4,"y":17},{"colour":"red","x":3,"y":17},{"colour":"red","x":2,"y":17},{"colour":"red","x":1,"y":17},{"colour":"red","x":0,"y":17},{"colour":"red","x":1,"y":16},{"colour":"red","x":2,"y":16},{"colour":"red","x":3,"y":16},{"colour":"red","x":4,"y":16},{"colour":"blue","x":5,"y":16},{"colour":"blue","x":6,"y":16},{"colour":"blue","x":7,"y":16},{"colour":"blue","x":8,"y":16},{"colour":"blue","x":9,"y":16},{"colour":"red","x":0,"y":16},{"colour":"red","x":0,"y":15},{"colour":"red","x":1,"y":15},{"colour":"red","x":2,"y":15},{"colour":"red","x":3,"y":15},{"colour":"red","x":4,"y":15},{"colour":"blue","x":5,"y":15},{"colour":"blue","x":6,"y":15},{"colour":"blue","x":7,"y":15},{"colour":"blue","x":8,"y":15},{"colour":"blue","x":9,"y":15},{"colour":"blue","x":9,"y":14},{"colour":"blue","x":8,"y":14},{"colour":"blue","x":8,"y":13},{"colour":"blue","x":7,"y":13},{"colour":"blue","x":6,"y":13},{"colour":"blue","x":5,"y":13},{"colour":"red","x":4,"y":14},{"colour":"red","x":3,"y":14},{"colour":"red","x":2,"y":14},{"colour":"red","x":1,"y":14},{"colour":"red","x":0,"y":14},{"colour":"blue","x":5,"y":14},{"colour":"blue","x":6,"y":14},{"colour":"blue","x":7,"y":14},{"colour":"blue","x":9,"y":13},{"colour":"red","x":4,"y":13},{"colour":"red","x":3,"y":13},{"colour":"red","x":2,"y":13},{"colour":"red","x":1,"y":13},{"colour":"red","x":0,"y":13},{"colour":"red","x":0,"y":12},{"colour":"red","x":1,"y":12},{"colour":"red","x":2,"y":12},{"colour":"red","x":3,"y":12},{"colour":"red","x":4,"y":12},{"colour":"blue","x":5,"y":12},{"colour":"blue","x":6,"y":12},{"colour":"blue","x":7,"y":12},{"colour":"blue","x":8,"y":12},{"colour":"blue","x":9,"y":12},{"colour":"blue","x":9,"y":11},{"colour":"blue","x":8,"y":11},{"colour":"blue","x":7,"y":11},{"colour":"blue","x":6,"y":11},{"colour":"blue","x":5,"y":11},{"colour":"red","x":4,"y":11},{"colour":"red","x":3,"y":11},{"colour":"red","x":2,"y":11},{"colour":"red","x":1,"y":11},{"colour":"red","x":0,"y":11},{"colour":"red","x":0,"y":10},{"colour":"red","x":1,"y":10},{"colour":"red","x":2,"y":10},{"colour":"red","x":3,"y":10},{"colour":"red","x":4,"y":10},{"colour":"blue","x":5,"y":10},{"colour":"blue","x":6,"y":10},{"colour":"blue","x":7,"y":10},{"colour":"blue","x":8,"y":10},{"colour":"blue","x":9,"y":10},{"colour":"blue","x":9,"y":9},{"colour":"blue","x":8,"y":9},{"colour":"blue","x":7,"y":9},{"colour":"blue","x":6,"y":9},{"colour":"blue","x":5,"y":9},{"colour":"red","x":4,"y":9},{"colour":"red","x":3,"y":9},{"colour":"red","x":2,"y":9},{"colour":"red","x":1,"y":9},{"colour":"red","x":0,"y":9},{"colour":"red","x":0,"y":8},{"colour":"red","x":0,"y":7},{"colour":"red","x":1,"y":7},{"colour":"red","x":2,"y":7},{"colour":"red","x":3,"y":7},{"colour":"red","x":4,"y":7},{"colour":"blue","x":5,"y":7},{"colour":"blue","x":6,"y":8},{"colour":"blue","x":7,"y":8},{"colour":"blue","x":8,"y":8},{"colour":"blue","x":9,"y":8},{"colour":"blue","x":5,"y":8},{"colour":"red","x":4,"y":8},{"colour":"red","x":3,"y":8},{"colour":"red","x":2,"y":8},{"colour":"red","x":1,"y":8},{"colour":"blue","x":6,"y":7},{"colour":"blue","x":7,"y":7},{"colour":"blue","x":8,"y":7},{"colour":"blue","x":9,"y":7},{"colour":"blue","x":9,"y":6},{"colour":"blue","x":8,"y":6},{"colour":"blue","x":7,"y":6},{"colour":"blue","x":6,"y":6},{"colour":"blue","x":5,"y":6},{"colour":"red","x":4,"y":6},{"colour":"red","x":3,"y":6},{"colour":"red","x":2,"y":6},{"colour":"red","x":1,"y":6},{"colour":"red","x":0,"y":6},{"colour":"red","x":0,"y":5},{"colour":"red","x":1,"y":5},{"colour":"red","x":2,"y":5},{"colour":"red","x":3,"y":5},{"colour":"red","x":4,"y":5},{"colour":"blue","x":5,"y":5},{"colour":"blue","x":6,"y":5},{"colour":"blue","x":7,"y":5},{"colour":"blue","x":8,"y":5},{"colour":"blue","x":9,"y":5},{"colour":"blue","x":8,"y":4},{"colour":"blue","x":7,"y":4},{"colour":"blue","x":6,"y":4},{"colour":"blue","x":5,"y":4},{"colour":"red","x":4,"y":4},{"colour":"red","x":3,"y":4},{"colour":"red","x":2,"y":4},{"colour":"red","x":1,"y":4},{"colour":"red","x":0,"y":4},{"colour":"blue","x":9,"y":4},{"colour":"blue","x":9,"y":3},{"colour":"blue","x":8,"y":3},{"colour":"blue","x":7,"y":3},{"colour":"blue","x":6,"y":3},{"colour":"blue","x":5,"y":3},{"colour":"red","x":4,"y":3},{"colour":"red","x":3,"y":3},{"colour":"red","x":2,"y":3},{"colour":"red","x":1,"y":3},{"colour":"red","x":0,"y":3},{"colour":"red","x":0,"y":2},{"colour":"red","x":1,"y":2},{"colour":"red","x":2,"y":2},{"colour":"blue","x":7,"y":2},{"colour":"blue","x":8,"y":2},{"colour":"blue","x":9,"y":2},{"colour":"blue","x":6,"y":2},{"colour":"blue","x":5,"y":2},{"colour":"red","x":4,"y":2},{"colour":"red","x":3,"y":2},{"colour":"red","x":1,"y":1},{"colour":"red","x":0,"y":1},{"colour":"red","x":2,"y":1},{"colour":"red","x":3,"y":1},{"colour":"red","x":4,"y":1},{"colour":"blue","x":5,"y":1},{"colour":"blue","x":6,"y":1},{"colour":"blue","x":7,"y":1},{"colour":"blue","x":8,"y":1},{"colour":"blue","x":9,"y":1},{"colour":"blue","x":9,"y":0},{"colour":"blue","x":8,"y":0},{"colour":"blue","x":7,"y":0},{"colour":"blue","x":6,"y":0},{"colour":"blue","x":5,"y":0},{"colour":"red","x":4,"y":0},{"colour":"red","x":3,"y":0},{"colour":"red","x":2,"y":0},{"colour":"red","x":1,"y":0},{"colour":"red","x":0,"y":0}],"in":3,"out":3}
+var cur_lab_part = {'in':0,'out':0, 'blocks':[], 'coins':[]}
+var full_lab_part = {'coins':[],"blocks":[{"colour":"blue","x":9,"y":19},{"colour":"blue","x":8,"y":19},{"colour":"blue","x":7,"y":19},{"colour":"blue","x":6,"y":19},{"colour":"blue","x":5,"y":19},{"colour":"red","x":4,"y":19},{"colour":"red","x":3,"y":19},{"colour":"red","x":2,"y":19},{"colour":"red","x":1,"y":19},{"colour":"red","x":0,"y":19},{"colour":"red","x":0,"y":18},{"colour":"red","x":1,"y":18},{"colour":"red","x":2,"y":18},{"colour":"red","x":3,"y":18},{"colour":"red","x":4,"y":18},{"colour":"blue","x":5,"y":18},{"colour":"blue","x":6,"y":18},{"colour":"blue","x":7,"y":18},{"colour":"blue","x":8,"y":18},{"colour":"blue","x":9,"y":18},{"colour":"blue","x":9,"y":17},{"colour":"blue","x":8,"y":17},{"colour":"blue","x":7,"y":17},{"colour":"blue","x":6,"y":17},{"colour":"blue","x":5,"y":17},{"colour":"red","x":4,"y":17},{"colour":"red","x":3,"y":17},{"colour":"red","x":2,"y":17},{"colour":"red","x":1,"y":17},{"colour":"red","x":0,"y":17},{"colour":"red","x":1,"y":16},{"colour":"red","x":2,"y":16},{"colour":"red","x":3,"y":16},{"colour":"red","x":4,"y":16},{"colour":"blue","x":5,"y":16},{"colour":"blue","x":6,"y":16},{"colour":"blue","x":7,"y":16},{"colour":"blue","x":8,"y":16},{"colour":"blue","x":9,"y":16},{"colour":"red","x":0,"y":16},{"colour":"red","x":0,"y":15},{"colour":"red","x":1,"y":15},{"colour":"red","x":2,"y":15},{"colour":"red","x":3,"y":15},{"colour":"red","x":4,"y":15},{"colour":"blue","x":5,"y":15},{"colour":"blue","x":6,"y":15},{"colour":"blue","x":7,"y":15},{"colour":"blue","x":8,"y":15},{"colour":"blue","x":9,"y":15},{"colour":"blue","x":9,"y":14},{"colour":"blue","x":8,"y":14},{"colour":"blue","x":8,"y":13},{"colour":"blue","x":7,"y":13},{"colour":"blue","x":6,"y":13},{"colour":"blue","x":5,"y":13},{"colour":"red","x":4,"y":14},{"colour":"red","x":3,"y":14},{"colour":"red","x":2,"y":14},{"colour":"red","x":1,"y":14},{"colour":"red","x":0,"y":14},{"colour":"blue","x":5,"y":14},{"colour":"blue","x":6,"y":14},{"colour":"blue","x":7,"y":14},{"colour":"blue","x":9,"y":13},{"colour":"red","x":4,"y":13},{"colour":"red","x":3,"y":13},{"colour":"red","x":2,"y":13},{"colour":"red","x":1,"y":13},{"colour":"red","x":0,"y":13},{"colour":"red","x":0,"y":12},{"colour":"red","x":1,"y":12},{"colour":"red","x":2,"y":12},{"colour":"red","x":3,"y":12},{"colour":"red","x":4,"y":12},{"colour":"blue","x":5,"y":12},{"colour":"blue","x":6,"y":12},{"colour":"blue","x":7,"y":12},{"colour":"blue","x":8,"y":12},{"colour":"blue","x":9,"y":12},{"colour":"blue","x":9,"y":11},{"colour":"blue","x":8,"y":11},{"colour":"blue","x":7,"y":11},{"colour":"blue","x":6,"y":11},{"colour":"blue","x":5,"y":11},{"colour":"red","x":4,"y":11},{"colour":"red","x":3,"y":11},{"colour":"red","x":2,"y":11},{"colour":"red","x":1,"y":11},{"colour":"red","x":0,"y":11},{"colour":"red","x":0,"y":10},{"colour":"red","x":1,"y":10},{"colour":"red","x":2,"y":10},{"colour":"red","x":3,"y":10},{"colour":"red","x":4,"y":10},{"colour":"blue","x":5,"y":10},{"colour":"blue","x":6,"y":10},{"colour":"blue","x":7,"y":10},{"colour":"blue","x":8,"y":10},{"colour":"blue","x":9,"y":10},{"colour":"blue","x":9,"y":9},{"colour":"blue","x":8,"y":9},{"colour":"blue","x":7,"y":9},{"colour":"blue","x":6,"y":9},{"colour":"blue","x":5,"y":9},{"colour":"red","x":4,"y":9},{"colour":"red","x":3,"y":9},{"colour":"red","x":2,"y":9},{"colour":"red","x":1,"y":9},{"colour":"red","x":0,"y":9},{"colour":"red","x":0,"y":8},{"colour":"red","x":0,"y":7},{"colour":"red","x":1,"y":7},{"colour":"red","x":2,"y":7},{"colour":"red","x":3,"y":7},{"colour":"red","x":4,"y":7},{"colour":"blue","x":5,"y":7},{"colour":"blue","x":6,"y":8},{"colour":"blue","x":7,"y":8},{"colour":"blue","x":8,"y":8},{"colour":"blue","x":9,"y":8},{"colour":"blue","x":5,"y":8},{"colour":"red","x":4,"y":8},{"colour":"red","x":3,"y":8},{"colour":"red","x":2,"y":8},{"colour":"red","x":1,"y":8},{"colour":"blue","x":6,"y":7},{"colour":"blue","x":7,"y":7},{"colour":"blue","x":8,"y":7},{"colour":"blue","x":9,"y":7},{"colour":"blue","x":9,"y":6},{"colour":"blue","x":8,"y":6},{"colour":"blue","x":7,"y":6},{"colour":"blue","x":6,"y":6},{"colour":"blue","x":5,"y":6},{"colour":"red","x":4,"y":6},{"colour":"red","x":3,"y":6},{"colour":"red","x":2,"y":6},{"colour":"red","x":1,"y":6},{"colour":"red","x":0,"y":6},{"colour":"red","x":0,"y":5},{"colour":"red","x":1,"y":5},{"colour":"red","x":2,"y":5},{"colour":"red","x":3,"y":5},{"colour":"red","x":4,"y":5},{"colour":"blue","x":5,"y":5},{"colour":"blue","x":6,"y":5},{"colour":"blue","x":7,"y":5},{"colour":"blue","x":8,"y":5},{"colour":"blue","x":9,"y":5},{"colour":"blue","x":8,"y":4},{"colour":"blue","x":7,"y":4},{"colour":"blue","x":6,"y":4},{"colour":"blue","x":5,"y":4},{"colour":"red","x":4,"y":4},{"colour":"red","x":3,"y":4},{"colour":"red","x":2,"y":4},{"colour":"red","x":1,"y":4},{"colour":"red","x":0,"y":4},{"colour":"blue","x":9,"y":4},{"colour":"blue","x":9,"y":3},{"colour":"blue","x":8,"y":3},{"colour":"blue","x":7,"y":3},{"colour":"blue","x":6,"y":3},{"colour":"blue","x":5,"y":3},{"colour":"red","x":4,"y":3},{"colour":"red","x":3,"y":3},{"colour":"red","x":2,"y":3},{"colour":"red","x":1,"y":3},{"colour":"red","x":0,"y":3},{"colour":"red","x":0,"y":2},{"colour":"red","x":1,"y":2},{"colour":"red","x":2,"y":2},{"colour":"blue","x":7,"y":2},{"colour":"blue","x":8,"y":2},{"colour":"blue","x":9,"y":2},{"colour":"blue","x":6,"y":2},{"colour":"blue","x":5,"y":2},{"colour":"red","x":4,"y":2},{"colour":"red","x":3,"y":2},{"colour":"red","x":1,"y":1},{"colour":"red","x":0,"y":1},{"colour":"red","x":2,"y":1},{"colour":"red","x":3,"y":1},{"colour":"red","x":4,"y":1},{"colour":"blue","x":5,"y":1},{"colour":"blue","x":6,"y":1},{"colour":"blue","x":7,"y":1},{"colour":"blue","x":8,"y":1},{"colour":"blue","x":9,"y":1},{"colour":"blue","x":9,"y":0},{"colour":"blue","x":8,"y":0},{"colour":"blue","x":7,"y":0},{"colour":"blue","x":6,"y":0},{"colour":"blue","x":5,"y":0},{"colour":"red","x":4,"y":0},{"colour":"red","x":3,"y":0},{"colour":"red","x":2,"y":0},{"colour":"red","x":1,"y":0},{"colour":"red","x":0,"y":0}],"in":3,"out":3}
 var selected_colour = 'auto'
 @onready var BlueBox = preload("res://blue_box.tscn")
 @onready var RedBox = preload("res://red_box.tscn")
+@onready var Coin = preload("res://coin.tscn")
 var last_path = ''
 
 # Called when the node enters the scene tree for the first time.
@@ -41,6 +42,23 @@ func create_block(pos, colour):
 	var pos_x = pos.y*target_bb_size
 	bb.position = Vector2(pos_x,poz_y)
 	add_child(bb)
+	
+func create_coin(pos):
+	var target_bb_size = cell_size
+	var bb = Coin.instantiate()
+	#var bb = BlueBox.instantiate()
+	#var bb = RedBox.instantiate()
+	var bb_size = bb.get_node('Sprite2D').get_rect().size.x
+	var s = target_bb_size/bb_size
+	bb.scale = Vector2(s,s)
+	var r_mob_size = target_bb_size
+	var poz_y = work_height - r_mob_size*(pos.x+1)
+	#var mob_size = get_viewport().get_visible_rect().size.y/10
+	var pos_x = pos.y*target_bb_size
+	bb.position = Vector2(pos_x,poz_y)
+	add_child(bb)
+
+
 
 func _draw():
 	work_width = get_viewport().get_visible_rect().size.x*0.8
@@ -55,16 +73,20 @@ func _draw():
 		
 	delete_blocks()
 	add_blocks()
+	add_coins()
 	$HUD.position.y = 0
 	$HUD.position.x = work_width
 	$HUD.size.y = work_height
 	$HUD.size.x = get_viewport().get_visible_rect().size.x - work_width
-	#print("draw")
+	print("draw")
 	
 	
 func is_block_exist(x:int,y:int):
 	var result = false
 	for i in cur_lab_part['blocks']:
+		if i['x'] == x and i['y'] == y:
+			result = i
+	for i in cur_lab_part['coins']:
 		if i['x'] == x and i['y'] == y:
 			result = i
 	return result
@@ -82,6 +104,10 @@ func delete_blocks():
 func add_blocks():
 	for b in cur_lab_part['blocks']:
 		create_block(Vector2(b['y'], b['x']), b['colour'])
+		
+func add_coins():
+	for b in cur_lab_part['coins']:
+		create_coin(Vector2(b['y'], b['x']))
 		
 		
 func _input(event):
@@ -102,16 +128,21 @@ func _input(event):
 				pass
 				#print ('already_exist')
 			else:
-				var colour_to_draw = ''
-				if selected_colour == 'auto':
-					if click_x<5:
-						colour_to_draw = 'red'
-					else:
-						colour_to_draw = 'blue'
+				if selected_colour == 'coin':
+					var new_block = {'x':click_x, 'y':click_y}
+					cur_lab_part['coins'].append(new_block)
+					print ('add_coin')
 				else:
-					colour_to_draw = selected_colour
-				var new_block = {'colour':colour_to_draw, 'x':click_x, 'y':click_y}
-				cur_lab_part['blocks'].append(new_block)
+					var colour_to_draw = ''
+					if selected_colour == 'auto':
+						if click_x<5:
+							colour_to_draw = 'red'
+						else:
+							colour_to_draw = 'blue'
+					else:
+						colour_to_draw = selected_colour
+					var new_block = {'colour':colour_to_draw, 'x':click_x, 'y':click_y}
+					cur_lab_part['blocks'].append(new_block)
 				#create_block(Vector2(click_y, click_x), 'red')
 			#print (cur_lab_part)
 		if event.as_text() == 'Right Mouse Button'or event.get_button_mask() == 2:
@@ -119,6 +150,7 @@ func _input(event):
 			delete_blocks()
 			if is_block_exist(click_x, click_y):
 				cur_lab_part['blocks'].erase(is_block_exist(click_x, click_y))
+				cur_lab_part['coins'].erase(is_block_exist(click_x, click_y))
 			else:
 				pass
 				#print ('not_exist')
@@ -129,6 +161,7 @@ func _input(event):
 
 func _on_clear_pressed():
 	cur_lab_part['blocks']=[]
+	cur_lab_part['coins']=[]
 	_draw()
 	pass # Replace with function body.
 
@@ -140,6 +173,8 @@ func _on_colour_item_selected(index):
 		selected_colour = 'red'
 	elif index == 2:
 		selected_colour = 'auto'
+	elif index == 3:
+		selected_colour = 'coin'
 	pass # Replace with function body.
 
 
@@ -168,6 +203,13 @@ func _on_file_dialog_file_selected(path):
 
 
 func _on_fill_pressed():
-	cur_lab_part = full_lab_part
+	print('fill')
+	print(full_lab_part)
+	cur_lab_part['blocks'] = []
+	cur_lab_part['coins'] = []
+	for b in full_lab_part['blocks']:
+		cur_lab_part['blocks'].append(b)
+	#var new_full_lab_part = full_lab_part
+	#cur_lab_part = full_lab_part
 	_draw()
 	pass # Replace with function body.
